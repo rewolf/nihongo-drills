d206312433f19cad0c34e00525055cdb2bd66c4c
rewolf_laptop <andrew.flower@gmail.com>
2011-11-11 00:05:41 +0200

    Forgot the new page files

---
+++ pages/character-drills/hiragana/from-katakana.php
@@ -0,0 +1,21 @@
+<div class="module" id="drill-char-kth">
+	<h1>Choose the hiragana character matching the katakana</h1>
+	<p class="drill-instruction">
+		This drill displays a random katakana character.  You must choose the corresponding hiragana character from the possibilities in the table below. Once the correct character is chosen, the next katakana character will be shown. The settings to the right can be used to use Google's alternate audio voice or to limit the range of characters being tested.
+	</p>
+	<div class="random-char-holder no-margin">
+	</div>
+	<div class="char-choice-holder no-margin">
+		<table width=80 height=80>
+		</table>
+	</div>
+	<div class="button-box inline">
+		<button class="play-button">Play</button>
+	</div>
+	<div class="button-box inline">
+		<button class="cheat-button drill-button">Cheat</button>
+	</div>
+	<div class="button-box inline">
+		<button class="next-button">Skip</button>
+	</div>
+</div>
---
+++ pages/character-drills/katakana/from-hiragana.php
@@ -0,0 +1,21 @@
+<div class="module" id="drill-char-htk">
+	<h1>Choose the katakana character matching the hiragana</h1>
+	<p class="drill-instruction">
+		This drill displays a random hiragana character.  You must choose the corresponding katakana character from the possibilities in the table below. Once the correct character is chosen, the next hiragana character will be shown. The settings to the right can be used to use Google's alternate audio voice or to limit the range of characters being tested.
+	</p>
+	<div class="random-char-holder no-margin">
+	</div>
+	<div class="char-choice-holder no-margin">
+		<table width=80 height=80>
+		</table>
+	</div>
+	<div class="button-box inline">
+		<button class="play-button">Play</button>
+	</div>
+	<div class="button-box inline">
+		<button class="cheat-button drill-button">Cheat</button>
+	</div>
+	<div class="button-box inline">
+		<button class="next-button">Skip</button>
+	</div>
+</div>
